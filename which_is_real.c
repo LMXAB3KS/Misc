c888f70523d738a6e0df661714330f4d2226ca99
LMXAB3KS <84719688+LMXAB3KS@users.noreply.github.com>
2023-12-20 17:17:52 +0530

    Create which_is_real.c

---
+++ which_is_real.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <conio.h>
+int main() {
+    // ANSI escape code for green text
+    printf("\033[1;32m");
+
+    // Text to be printed in green
+    printf("\n...Program finished with exit code 0\nPress ENTER to exit console.\n");
+    printf("\n...Program finished with exit code 0\nPress ENTER to exit console.\n");
+    printf("\n...Program finished with exit code 0\nPress ENTER to exit console.\n");
+    printf("\n...Program finished with exit code 0\nPress ENTER to exit console.\n");
+    printf("\n...Program finished with exit code 0\nPress ENTER to exit console.\n");
+    printf("\n...Program finished with exit code 0\nPress ENTER to exit console.\n");
+    printf("\n...Program finished with exit code 0\nPress ENTER to exit console.\n");
+    printf("\n...Program finished with exit code 0\nPress ENTER to exit console.\n");
+    printf("\n...Program finished with exit code 0\nPress ENTER to exit console.\n");
+    printf("\n...Program finished with exit code 0\nPress ENTER to exit console.\n");
+    printf("\n...Program finished with exit code 0\nPress ENTER to exit console.\n");
+    printf("\n...Program finished with exit code 0\nPress ENTER to exit console.\n");
+    printf("\n...Program finished with exit code 0\nPress ENTER to exit console.\n");
+    printf("\n...Program finished with exit code 0\nPress ENTER to exit console.\n");
+    printf("\n...Program finished with exit code 0\nPress ENTER to exit console.\n");
+    
+   
+    // Reset text color to default
+    printf("\033[0m");
+
+    getch();
+}
